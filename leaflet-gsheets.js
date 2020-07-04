fbe6b2fe978b9b7befabbd0865930737b206aed4
thanoskyr <thanoskyr219@gmail.com>
2020-07-04 19:00:22 +0300

    final

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -10,10 +10,9 @@ function init() {
   // PASTE YOUR URLs HERE
   // these URLs come from Google Sheets 'shareable link' form
   // the first is the polygon layer and the second the points
-  var polyURL = "https://drive.google.com/file/d/1ahc9gcMUaPq3zBK0wQbYgUmgCysVsh5g/view?usp=sharing";
-   // "https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing";
-  var pointsURL =
-    "https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing";
+  var polyURL = "https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing";
+  var pointsURL ="https://docs.google.com/spreadsheets/d/1mIEF-1mwioK0a7lFwAQjC2dyqe2h0r0xUmIM1gXNvws/edit?usp=sharing";
+  //  "https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing";
 
   Tabletop.init({ key: polyURL, callback: addPolygons, simpleSheet: true });
   Tabletop.init({ key: pointsURL, callback: addPoints, simpleSheet: true }); // simpleSheet assumes there is only one table and automatically sends its data
